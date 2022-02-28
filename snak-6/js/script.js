9e167077ec588f79c72f5987e45fd4ecfe9ca23f
MattiaPortanti <mattia.portantipm@gmail.com>
2022-02-28 19:02:06 +0100

    corretto snak 6

--- snak-6/js/script.js
+++ snak-6/js/script.js
@@ -5,5 +5,19 @@ e calcola la somma di tutte le cifre che compongono il numero.
  *
  */
 
-const userNumber = parseInt(prompt("Inserisci un numero di sette cifre"));
+//! Inizializzo una costante con il valore del numero inserito dall'utente nel prompt
+const userNumber = prompt("Inserisci un numero di sette cifre");
+//! variabile che sad ogni ricaricamento si setta a 0
+let sum = 0;
+
+//! ciclo la i per la lunghezza dell USERNUMBER 
+for (let i = 0; i < userNumber.length; i = i + 1){
+    //! Per ogni giro la variabile SUM somma se stessa al valore del carattere successivo e lo fà per tutti i caratteri 
+    //! che compongono USERNUMBER.
+    sum += parseInt(userNumber.charAt(i));
+}
+
+//! infine stampa il valore finale sulla console
+console.log(sum);
+
 
